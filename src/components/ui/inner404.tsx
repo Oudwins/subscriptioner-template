a3a5f7a8b726935c286eb44f529f32d1c8db2a49
= <tristanmayofreire@gmail.com>
2023-06-13 00:18:15 +0200

    fixed error

--- src/components/ui/inner404.tsx
+++ src/components/ui/inner404.tsx
@@ -48,8 +48,8 @@ export default () => {
                 className="h-5 w-5 rtl:rotate-180"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
                 />
               </svg>
