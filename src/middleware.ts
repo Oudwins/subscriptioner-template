255fe26bddbeeb1febe12065c9669b0289ff2af8
= <tristanmayofreire@gmail.com>
2023-06-12 09:12:20 +0200

    fixed clerk redict hooks so they didn't arrive

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,12 @@ import { authMiddleware } from "@clerk/nextjs";
 //   publicRoutes: ["/", "/api/(.*)"],
 // });
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/stripe-hooks"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  //matcher: ["/((?!.*\\..*|_next).*)", "/"],
   //matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)", "/"],
 };
