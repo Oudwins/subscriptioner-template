6aada3719fc92abe9fa7e9585bd319826ce619fc
= <tristanmayofreire@gmail.com>
2023-06-12 14:55:00 +0200

    Basic subscriptions table created

---
+++ src/components/dashboard/SubscriptionTable.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { SubscriptionSchema } from "~/db/schema";
+import { formatUnixTimestamp, formatCurrency } from "~/utils/format";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { MoreHorizontal, Files, XCircle } from "lucide-react";
+import Link from "next/link";
+
+export type Subscription = SubscriptionSchema;
+
+// export type Subscription = {
+//   status: string;
+//   currentPeriodStart: number;
+//   currentPeriodEnd: number;
+//   price: number;
+// };
+
+export const columns: ColumnDef<Subscription>[] = [
+  // {
+  //   accessorKey: "description",
+  //   header: "descripción",
+  // },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "currentPeriodStart",
+    header: "Comienzo",
+    cell: ({ row }) => {
+      const timestamp = parseInt(row.getValue("currentPeriodStart"));
+      return formatUnixTimestamp(timestamp);
+    },
+  },
+  {
+    accessorKey: "currentPeriodEnd",
+    header: "Expiración",
+    cell: ({ row }) => {
+      const timestamp = parseInt(row.getValue("currentPeriodEnd"));
+      return formatUnixTimestamp(timestamp);
+    },
+  },
+  {
+    accessorKey: "price",
+    header: () => <div className="">Cantidad</div>,
+    cell: ({ row }) => {
+      const amount = parseInt(row.getValue("price"));
+      const currency = row.original.currency || "EUR";
+      console.log(row.original.currency);
+      const locale = navigator.language;
+      return (
+        <div className="font-medium">
+          {formatCurrency({
+            amount,
+            currency,
+            locale,
+          })}
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu className="">
+          <DropdownMenuTrigger asChild className="flex justify-end">
+            <div className="self-end">
+              <Button variant="ghost" className=" p-0">
+                <span className="sr-only">Abrir menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56 bg-white">
+            <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer rounded transition-all hover:bg-indigo-50"
+              asChild
+            >
+              <Link
+                href={`/dashboard/subscriptions/${row.original.stripePriceId}/invoices`}
+              >
+                <Files className="mr-2 h-4 w-4" />
+                <span>Ver facturas</span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer rounded transition-all hover:bg-red-100"
+              asChild
+            >
+              <Link
+                href={`/dashboard/subscriptions/${row.original.stripePriceId}/cancel`}
+              >
+                <XCircle className="mr-2 h-4 w-4" />
+                <span>Cancelar</span>
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
