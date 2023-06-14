0315097907f9fa353acee33d81b3e304ab6c1348
= <tristanmayofreire@gmail.com>
2023-06-14 14:43:32 +0200

    added favicons to layouts

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -40,7 +40,29 @@ export default ({ children }: { children: ReactNode }) => {
           name="description"
           content="Inicia sesión para gestionar tus subscripciones"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       {/* NAVBAR */}
       <nav className="w-full border-b bg-white md:static md:border-none md:text-sm">
